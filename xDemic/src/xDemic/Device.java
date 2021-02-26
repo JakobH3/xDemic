eeb087f4a3ace9c2c907200236e643713eaeb024
TJ <tjtractorboy@gmail.com>
2021-02-26 01:08:32 -0500

    Setters, getters, constructors, and random generation
    This also includes some demonstration of the functions, creating malware and devices, and getting information from those.

--- xDemic/src/xDemic/Device.java
+++ xDemic/src/xDemic/Device.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 
 public class Device {
 	private int ID; //identifier for the device
-	private ArrayList<Malware> malwareList; //list of all malware active on the device
+	private ArrayList<Malware> malwareList = new ArrayList<Malware>(); //list of all malware active on the device
 	
-	public Device() {
-		// CONSTRUCTOR
+	public Device(int nextID) {
+		ID=nextID;
 	}
 	
 	public int getID() {
@@ -24,11 +24,23 @@ public class Device {
 	
 	public void infect(Malware malware) {
 		malwareList.add(malware);
-		System.out.println("Device " + ID + " Infected!");
+		System.out.println("Device " + ID + " infected!\n");
 	}
 	
 	public void patch(Malware malware) {
 		malwareList.remove(malware);
-		System.out.println("Device " + ID + " Patched!");
+		System.out.println("Device " + ID + " patched!\n");
+	}
+	
+	public void printInfo() {
+		System.out.println("ID: " + ID);
+		if(malwareList.size()==0) {
+			System.out.println("Device " + ID + " is not infected.\n");
+		} else {
+			System.out.println("Device " + ID + " is infected with:");
+			for(int i=0; i<malwareList.size(); i++) {
+				System.out.println("\t" + (malwareList.get(i)).getName());
+			}
+		}
 	}
 }
--- xDemic/src/xDemic/xDemic.java
+++ xDemic/src/xDemic/xDemic.java
@@ -3,10 +3,23 @@ package xDemic;
 import java.util.ArrayList;
 
 public class xDemic {
-	private ArrayList<Device> deviceList;
-	private ArrayList<Malware> malwareList;
-	
 	public static void main(String args[]) {
-		System.out.println("Welcome to xDemic!");
+		ArrayList<Device> deviceList = new ArrayList<Device>();
+		ArrayList<Malware> malwareList = new ArrayList<Malware>();
+		
+		System.out.println("== Welcome to xDemic! ==\n");
+		
+		Malware testMW1 = new Malware();
+		malwareList.add(testMW1);
+		testMW1.printInfo();
+		
+		Malware testMW2 = new Malware("test2", 90, 6);
+		malwareList.add(testMW2);
+		testMW2.printInfo();
+		
+		Device testD1 = new Device(0);
+		testD1.infect(testMW1);
+		testD1.infect(testMW2);
+		testD1.printInfo();
 	}
 }
