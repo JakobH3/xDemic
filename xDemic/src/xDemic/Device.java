962f7e319ee65ca8e814cdd9c0f1e363230b89c0
TJ <tjtractorboy@gmail.com>
2021-02-26 19:37:10 -0500

    Small improvements to basic functions

--- xDemic/src/xDemic/Device.java
+++ xDemic/src/xDemic/Device.java
@@ -24,23 +24,29 @@ public class Device {
 	
 	public void infect(Malware malware) {
 		malwareList.add(malware);
-		System.out.println("Device " + ID + " infected!\n");
+		System.out.println(">  Device " + ID + " infected with " + malware.getName() + "!\n");
 	}
 	
 	public void patch(Malware malware) {
-		malwareList.remove(malware);
-		System.out.println("Device " + ID + " patched!\n");
+		if(malwareList.remove(malware)) {
+			System.out.println(">  Device " + ID + " patched " + malware.getName() + "!\n");
+		} else {
+			System.out.println(">  Device " + ID + " is not infected with " + malware.getName() + ".\n");
+		}
+		
 	}
 	
 	public void printInfo() {
-		System.out.println("ID: " + ID);
+		System.out.println("== Device Info =========================");
+		System.out.println("|  ID: " + ID);
 		if(malwareList.size()==0) {
-			System.out.println("Device " + ID + " is not infected.\n");
+			System.out.println("|  Device " + ID + " is not infected.");
 		} else {
-			System.out.println("Device " + ID + " is infected with:");
+			System.out.println("|  Device " + ID + " is infected with:");
 			for(int i=0; i<malwareList.size(); i++) {
-				System.out.println("\t" + (malwareList.get(i)).getName());
+				System.out.println("|  - " + (malwareList.get(i)).getName());
 			}
 		}
+		System.out.println("========================================\n");
 	}
 }
--- xDemic/src/xDemic/xDemic.java
+++ xDemic/src/xDemic/xDemic.java
@@ -7,7 +7,7 @@ public class xDemic {
 		ArrayList<Device> deviceList = new ArrayList<Device>();
 		ArrayList<Malware> malwareList = new ArrayList<Malware>();
 		
-		System.out.println("== Welcome to xDemic! ==\n");
+		System.out.println("== Welcome to xDemic! ==================\n");
 		
 		Malware testMW1 = new Malware();
 		malwareList.add(testMW1);
@@ -17,9 +17,25 @@ public class xDemic {
 		malwareList.add(testMW2);
 		testMW2.printInfo();
 		
-		Device testD1 = new Device(0);
+		/*Device testD1 = new Device(0);
+		deviceList.add(testD1);
+		
 		testD1.infect(testMW1);
 		testD1.infect(testMW2);
-		testD1.printInfo();
+		testD1.patch(testMW2);
+		testD1.patch(testMW2);
+		testD1.printInfo();*/
+		
+		for(int i=0; i<50; i++) {
+			deviceList.add(new Device(i));
+		}
+		
+		deviceList.get(0).infect(testMW1);
+		deviceList.get(1).infect(testMW2);
+		deviceList.get(2).infect(testMW1);
+		deviceList.get(2).printInfo();
+		deviceList.get(2).patch(testMW1);
+		deviceList.get(2).patch(testMW1);
+		deviceList.get(49).printInfo();
 	}
 }
