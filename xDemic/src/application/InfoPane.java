e453684446d57611426dfb2676f4367c4a1069bf
JakobH3 <60677704+JakobH3@users.noreply.github.com>
2021-04-22 18:13:07 -0400

    added info

--- xDemic/src/application/InfoPane.java
+++ xDemic/src/application/InfoPane.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.awt.Insets;
 import java.util.ArrayList;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.GridPane;
@@ -15,46 +16,51 @@ public class InfoPane extends Pane {
 		setMinHeight(200);
 		getChildren().add(pane);
 	}
-
-	public void update() {
-		setPrefHeight(mainView.getHeight()*1/3);
-		// TODO may want to change infopane to extend scrollpane or some other method,
-		//      then put a vbox or hbox inside it
-		
-		// TODO display mobility rates
-		// TODO display frames per second
-		// TODO display total devices
-		// TODO display list of malware with % infected and % patched
-		// TODO display number of nodes
-		// TODO display number of connections
-		
-		// TODO if something specific is selected, display related info
-		// ** this will probably come later ** //
-		
-		int numDevices = mainView.getSimulation().getDeviceList().size();
+	
+	public void update() 
+	{
 		GridPane tempPane = new GridPane();
 		ArrayList<Device> temp = mainView.getSimulation().getDeviceList();
-		temp.addAll(mainView.getSimulation().getNodeList());
+		int numDevices = mainView.getSimulation().getDeviceList().size();
+		int numNodes = mainView.getSimulation().getNodeList().size();
+		int numConnections = mainView.getSimulation().getConnectionList().size();
+		int frameRate = mainView.getSimulator().getFrameRate();
 		double perInfVal = mainView.getSimulation().calculatePercentInf(temp);
+		
+		
 		ArrayList<Malware> malList = mainView.getSimulation().getMalwareList();
 		
 		pane.getChildren().clear();
 		pane.setPrefHeight(mainView.getHeight()*1/3);
 		
-		Text numDeviceText = new Text("Percentage Infected: " + perInfVal + "%");
+		Text numDeviceText = new Text(" Number of Devices: " + numDevices);
+		Text numNodesText = new Text(" Number of Nodes " + numNodes);
+		Text numConnectionsText = new Text(" Number of Connections " + numConnections);
+		Text frameRateText = new Text(" Frame Rate: " + frameRate);
 		
-		Text perInf = new Text(" Number of Devices: " + numDevices);
-		tempPane.add(perInf,1,0);
+		Text perInf = new Text(" Percentage Infected: " + perInfVal + "%");
+		
 		tempPane.add(numDeviceText,0,0);
+		tempPane.add(numNodesText,0,1);
+		tempPane.add(numConnectionsText,0,2);
+		tempPane.add(frameRateText,0,3);
+		
+		
+		
+		tempPane.add(perInf,1,0);
+		
+		
 		
 		
 		for(int i = 0; i < malList.size(); i++)
 		{
 			double tempVal = mainView.getSimulation().calculatePercentRecovered(temp, malList.get(i));
-			Text tempText = new Text("Percent of Devices Recovered From " + malList.get(i).getName() + " is " + tempVal + "%");
-			tempPane.add(tempText,0,i + 1);
+			Text tempText = new Text(" Percent of Devices Recovered From " + malList.get(i).getName() + " is " + tempVal + "%");
+			tempPane.add(tempText,1,i+1);
 		}
 		
 		pane.getChildren().add(tempPane);
+		
+		
 	}
 }
